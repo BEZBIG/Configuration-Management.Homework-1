d73932c32bca05df8ed262139fd61865a5716923
BEZBIG <bezbig@yandex.ru>
2024-10-10 21:14:43 +0300

    Add def load_confing

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ class EmulatorShell:
         self.load_config(config_file)  # Загружаем конфигурационный файл
         self.load_filesystem()  # Загружаем виртуальную файловую систему из архива
 
+    # Загружаем конфигурацию из XML-файла
+    def load_config(self, config_file):
+        tree = ET.parse(config_file)  # Парсим XML-файл
+        root = tree.getroot()  # Получаем корневой элемент
+        self.hostname = root.find("hostname").text  # Извлекаем имя компьютера
+        self.fs_path = root.find("filesystem").text  # Извлекаем путь к файловой системе
 
