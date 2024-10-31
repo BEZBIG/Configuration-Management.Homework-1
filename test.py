80e344738e4e0cb9ed8ea7058a4aaf35baf3694a
BEZBIG <bezbig@yandex.ru>
2024-10-31 20:59:57 +0300

    Changing tests of EmulatorShell

--- test.py
+++ test.py
@@ -1,36 +1,54 @@
-import unittest
+import pytest
 import os
+import zipfile
+import xml.etree.ElementTree as ET
 from main import EmulatorShell
-from pathlib import Path
 
 
-class TestShellEmulator(unittest.TestCase):
-    def setUp(self):
-        self.emulator = EmulatorShell('config.xml')
+def test_ls():
+    emulator = EmulatorShell('config.xml')
+    # Перенаправляем вывод в строку для тестирования
+    from io import StringIO
+    import sys
+    old_stdout = sys.stdout
+    sys.stdout = StringIO()
 
-    def test_ls(self):
-        self.emulator.current_dir = '/tmp/test_files'
-        output = os.listdir(self.emulator.current_dir)
-        self.assertIn('main.js', output)  # Тест на наличие файла
+    emulator.ls()
+    output = sys.stdout.getvalue().strip().split('\n')
+    sys.stdout = old_stdout
 
-    def test_cd(self):
-        self.emulator.cd('Left')
-        expected_path = Path('/tmp/test_files/Left')
-        self.assertEqual(Path(self.emulator.current_dir), expected_path)
+    assert sorted(output) == sorted(['Left', 'Right', 'main.js'])
 
-    def test_rm(self):
-        test_file = os.path.join(self.emulator.current_dir, 'testfile.txt')
-        open(test_file, 'w').close()  # Создаем тестовый файл
-        self.emulator.rm('testfile.txt')
-        self.assertFalse(os.path.exists(test_file))
 
-    def test_cp(self):
-        src_file = os.path.join(self.emulator.current_dir, 'testfile.txt')
-        dest_file = os.path.join(self.emulator.current_dir, 'copyfile.txt')
-        open(src_file, 'w').close()  # Создаем тестовый файл
-        self.emulator.cp('testfile.txt', 'copyfile.txt')
-        self.assertTrue(os.path.exists(dest_file))
+def test_cd():
+    emulator = EmulatorShell('config.xml')
+    emulator.cd('Left')
+    assert emulator.current_dir == emulator.fs_structure['Left']
+    assert emulator.path == '/Left'
+
+    emulator.cd('..')
+    assert emulator.current_dir == emulator.fs_structure
+    assert emulator.path == '/'
+
+    # Тестирование ошибки перехода в несуществующую директорию
+    emulator.cd('non_existent')
+    assert emulator.path == '/'
+
+
+def test_rm():
+    emulator = EmulatorShell('config.xml')
+    emulator.cd('Left')
+    emulator.rm('file1.txt')
+    assert 'file1.txt' not in emulator.current_dir
+    emulator.rm('non_existent')  # тестируем удаление несуществующего файла
+
+
+def test_cp():
+    emulator = EmulatorShell('config.xml')
+    emulator.cd('Left')
+    emulator.cp('file1.txt', 'file1_copy.txt')
+    assert 'Left.gif' in emulator.current_dir
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
